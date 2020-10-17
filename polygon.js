17b6a0abf7f496e0800d90abee96901397c26f1c
Harshita1474 <72028497+Harshita1474@users.noreply.github.com>
2020-10-17 18:07:50 +0530

    Update polygon.js

--- polygon.js
+++ polygon.js
@@ -1,15 +1,14 @@
 class Polygon {
     constructor(x,y,width,height) {
-      this.body = Bodies.rectangle(x,y,width,height);
-      this.width = width;
-      this.height = height;
+      this.body = Bodies.circle(x,y,radius);
+      this.radius = radius;
       World.add(world, this.body);
     }
     display(){
       var pos =this.body.position;
-      rectMode(CENTER);
+      ellipseMode(CENTER);
       fill("brown");
-      rect(pos.x, pos.y, this.width, this.height);
+      ellipse(pos.x, pos.y, this.width, this.height);
     }
   };
   
